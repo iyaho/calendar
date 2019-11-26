dccf1d290960f32239957064f9b3154ac45ea070
unknown <dlwlsgh0808@gmail.com>
2019-11-26 20:29:11 +0900

    main

---
+++ src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+export default withLayout(Main);
